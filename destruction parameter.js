bf723de6f0d2dff3c117fd34ea7260fb7e7dc3b2
freya0608 <freya001@yeah.net>
2017-03-03 17:22:00 +0800

    name

--- destruction parameter.js
+++ destruction parameter.js
@@ -1,4 +1,9 @@
 /**
  * Created by freya on 2017/3/3.
  */
+'use strict';
+function breakfast(dessert, drink, {location, restaurant}={}) {
+    console.log(dessert,drink,location,restaurant);//cake 饮料 济南 董小姐
+}
+breakfast('cake','饮料',{location:'济南',restaurant:'董小姐'});
 
---
+++ name.js
@@ -0,0 +1,18 @@
+/**
+ * Created by Administrator on 2017/3/3.
+ */
+
+// function breakfast(argument) {
+//
+// }
+// console.log(breakfast.name);//breakfast
+
+let breakfast = function superBreakfast (argument){
+
+};
+console.log(breakfast.name);//superBreakfast
+
+// var breakfast = function superBreakfast (argument){
+//
+// };
+// console.log(breakfast.name);//superBreakfast
