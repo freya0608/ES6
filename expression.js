a0b73e5b087a21f3d9bd2a619d2610f5a3a4a958
freya0608 <freya001@yeah.net>
2017-03-03 18:07:34 +0800

    express

---
+++ expression.js
@@ -0,0 +1,21 @@
+/**
+ * Created by Administrator on 2017/3/3.
+ */
+'use strict';
+let dessert ='cake',drink='tee';
+let food = {
+    // dessert:dessert,
+    // drink:drink
+    // breakfast:function () {
+    //
+    // },
+
+    dessert:dessert,
+    drink:drink,
+    breakfast(){}
+};
+console.log(food);
+// Object
+// dessert:"cake"
+// drink: "tee"
+// __proto__: Object
