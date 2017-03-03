977a343002c72ee7436727db43e09c23c40f2da8
freya0608 <freya001@yeah.net>
2017-03-03 17:36:11 +0800

    arrow function

---
+++ arrow functions.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Administrator on 2017/3/3.
+ */
+'use strict';
+// var breakfast =function breakfast(dessert) {
+//     return dessert;
+// }
+let breakfast = (dessert,drink) => {
+   return dessert +drink;
+};
