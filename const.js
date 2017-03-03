61ee049de4069538106e3b4ca405dfd6362ba971
freya0608 <freya001@yeah.net>
2017-03-03 11:15:25 +0800

    let   add   const

---
+++ const.js
@@ -0,0 +1,11 @@
+"use strict"
+const fruit = '苹果';
+console.log(fruit);
+
+const fruit = '柠檬';
+console.log(fruit);//不可以
+
+const fruit = [];
+fruit.push('苹果');
+fruit.push('苹果');
+console.log(fruit);//可以
---
+++ let.js
@@ -0,0 +1,6 @@
+"use strict"
+if (true) {
+	// var fruit = '苹果';
+	let fruit = '苹果';
+}
+console.log(fruit); 
